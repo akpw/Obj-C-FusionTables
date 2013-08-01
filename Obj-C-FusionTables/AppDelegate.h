0022424b292121ea55960be5d547ae11a4c6428d
Arseniy Kuznetsov <k.arseniy@gmail.com>
2013-08-01 08:49:26 +0200

    Initial commit

--- Obj-C-FusionTables/AppDelegate.h
+++ Obj-C-FusionTables/AppDelegate.h
@@ -2,18 +2,15 @@
 //  AppDelegate.h
 //  Obj-C-FusionTables
 //
-//  Created by Arseniy on 1/8/13.
+//  Created by Arseniy on 23/7/13.
 //  Copyright (c) 2013 Arseniy Kuznetsov. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@class ViewController;
-
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
-@property (strong, nonatomic) ViewController *viewController;
+@property (strong, nonatomic) UINavigationController *navigationController;
 
 @end
